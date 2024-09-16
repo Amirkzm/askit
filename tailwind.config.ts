d44bdb80eed797bf311e7603e4c04af84967f450
amir.kzm <kazemi.amir68@gmail.com>
2024-09-16 22:07:48 +0200

    first commit. project setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,70 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          100: "#fdf2ea",
+          200: "#fbe5d5",
+          300: "#f9d8c1",
+          400: "#f7cbac",
+          500: "#f6be98",
+          600: "#f4b183",
+          700: "#f2a46e",
+          800: "#8f4db3",
+          900: "#ed7d31",
+        },
+        dark: {
+          100: "#1a1a1a",
+          200: "#333333",
+          300: "#4d4d4d",
+          400: "#666666",
+          500: "#808080",
+          600: "#999999",
+          700: "#b3b3b3",
+          800: "#cccccc",
+          900: "#e6e6e6",
+        },
+      },
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+        spaceGrotesk: ["var(--font-spaceGrotesk)"],
+      },
+      boxShadow: {
+        "light-100":
+          "0px 12px 20px 0px rgba(184, 184, 184, 0.03), 0px 6px 12px 0px rgba(184, 184, 184, 0.02), 0px 2px 4px 0px rgba(184, 184, 184, 0.03)",
+        "light-200": "10px 10px 20px 0px rgba(218, 213, 213, 0.10)",
+        "light-300": "-10px 10px 20px 0px rgba(218, 213, 213, 0.10)",
+        "dark-100": "0px 2px 10px 0px rgba(46, 52, 56, 0.10)",
+        "dark-200": "2px 0px 20px 0px rgba(39, 36, 36, 0.04)",
+      },
+      backgroundImage: {
+        "auth-dark": "url('/assets/images/auth-dark.png')",
+        "auth-light": "url('/assets/images/auth-light.png')",
+      },
+      screens: {
+        xs: "420px",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
