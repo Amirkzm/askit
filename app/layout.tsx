b3a414ea138efdddb04d26481b082ba8b5f45e34
amir.kzm <kazemi.amir68@gmail.com>
2024-09-17 13:44:47 +0200

    configure and setut layout metadata and icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,32 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
-
 import "./globals.css";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "AskIt",
+  description:
+    "A place to ask and answer to questions in some fields like, computer, health, engineering, and others",
+
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
+};
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
 export default function RootLayout({
   children,
 }: {
@@ -15,9 +39,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           <SignedOut>
-            <SignInButton />
+            <SignInButton>
+              <button>Sign in with Clerk</button>
+            </SignInButton>
             <h1>Root Layout kos</h1>
           </SignedOut>
           <SignedIn>
