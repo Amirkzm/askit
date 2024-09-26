67f0ff2d0533b07437d352d53457fba53524e1c7
amir.kzm <kazemi.amir68@gmail.com>
2024-09-26 15:14:36 +0200

    Complete unanswered page. use mostly from the questions page component because of similarity between two

--- app/(root)/unanswered/page.tsx
+++ app/(root)/unanswered/page.tsx
@@ -1,7 +1,53 @@
+import QuestionCard from "@/components/shared/questions/QuestionCard";
+import GlobalSearch from "@/components/shared/search/GlobalSearch";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { tempLatestQuestions } from "@/constants/tempConst";
 import React from "react";
 
 const page = () => {
-  return <div>unanswered</div>;
+  return (
+    <section>
+      <div className="flex-between items-center mb-8">
+        <h2 className="text-2xl font-bold">Unanswered Questions</h2>
+        <Button className=" base-bold !bg-primary-500">Ask a Question</Button>
+      </div>
+      <div className="flex-between gap-16 ">
+        <GlobalSearch className="flex-1" />
+        <Select>
+          <SelectTrigger className="w-[180px] !h-[50px]">
+            <SelectValue placeholder="Theme" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="light">Light</SelectItem>
+            <SelectItem value="dark">Dark</SelectItem>
+            <SelectItem value="system">System</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+      <div>
+        {tempLatestQuestions.map((question) => {
+          return (
+            <QuestionCard
+              owner={question.owner}
+              question={question.title}
+              tags={question.tags}
+              key={question.id}
+              view={question.views}
+              vote={question.votes}
+              answer={question.answers}
+            />
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default page;
